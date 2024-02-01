cd067250c45a2aae96706c95165c115c860b671b
Andrewtsy <68668336+Andrewtsy@users.noreply.github.com>
2024-02-01 21:25:36 +0000

    Deploying to public from @ Andrewtsy/andrewtsy.github.io@542010bb20e13fa9eb6dd8043d5222b0a176f146 🚀

---
+++ _next/static/QiDJIP8EsBp-n1q9LR4qO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/107f5b861ef8f8e3.css","static/chunks/pages/index-aeb6d7a2c114fac4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/UWiJG6sBx99tuZk6MHDM_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/107f5b861ef8f8e3.css","static/chunks/pages/index-483773e6f1b8e7ac.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
